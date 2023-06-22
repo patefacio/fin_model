9b3fc2428003fbb2f6490950df3bb508a383dda8
Faith Smith <faithsmithcodingwork@gmail.com>
2023-06-22 08:53:56 -0500

    Added YearValue & YearRange Input Fields

--- rust/gui/src/component/year_range_input.rs
+++ rust/gui/src/component/year_range_input.rs
@@ -25,9 +25,84 @@ pub fn YearRangeInput(
     updatable: Updatable<Option<YearRange>>,
 ) -> impl IntoView {
     // α <fn year_range_input>
+    use crate::YearInput;
+    use std::cell::RefCell;
+    use std::rc::Rc;
+
+    let updatable = Rc::new(RefCell::new(updatable));
+
+    let initial_start_year = updatable
+        .as_ref()
+        .borrow()
+        .value
+        .as_ref()
+        .map(|option_of_year_range| option_of_year_range.start);
+
+    let initial_end_year = updatable
+        .as_ref()
+        .borrow()
+        .value
+        .as_ref()
+        .map(|option_of_year_range| option_of_year_range.end);
+
+    let updatable_for_start_year = Rc::clone(&updatable);
+    let start_year_updatable = Updatable::new(initial_start_year, move |initial_start_year| {
+        if let Some(initial_start_year) = initial_start_year.clone() {
+            updatable_for_start_year
+                .as_ref()
+                .borrow_mut()
+                .update_and_then_signal(|year_range| {
+                    if let Some(year_range) = year_range {
+                        year_range.start = initial_start_year;
+                        
+                    }; 
+                });
+        }
+    });
+
+    let updatable_for_end_year = Rc::clone(&updatable);
+    let end_year_updatable = Updatable::new(initial_end_year, move |initial_end_year| {
+        if let Some(initial_end_year) = initial_end_year.clone() {
+            updatable_for_end_year
+                .as_ref()
+                .borrow_mut()
+                .update_and_then_signal(|year_range| {
+                    if let Some(year_range) = year_range {
+                        year_range.end = initial_end_year;
+                        
+                    };
+                });
+        }
+    });
+
     view! {
         cx,
         <h3>"TODO Year Range"</h3>
+
+        <fieldset class="nsg">
+        <legend>"Year Range"</legend>
+        <div class="form">
+
+
+            <div style="display: inline-flex" >
+
+                "("
+                    <YearInput
+                        placeholder=Some("start".to_string())
+                        updatable=start_year_updatable
+                    />
+                ","
+                    <YearInput
+                        placeholder=Some("end".to_string())
+                        updatable=end_year_updatable
+
+                    />
+                ")"
+
+            </div>
+
+        </div>
+        </fieldset>
     }
     // ω <fn year_range_input>
 }
