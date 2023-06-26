7023863161b86309741209657b24b5117aad39af
Faith Smith <faithsmithcodingwork@gmail.com>
2023-06-26 09:35:46 -0500

    added use statements for buttons

--- rust/gui/src/component/rate_curve_component.rs
+++ rust/gui/src/component/rate_curve_component.rs
@@ -8,6 +8,15 @@ use leptos::{component, view, IntoView, Scope};
 #[allow(unused_imports)]
 use leptos_dom::console_log;
 use plus_modeled::RateCurve;
+use plus_modeled::YearValue;
+use crate::YearValueInput;
+use leptos_dom::html::{Button, Div};
+use std::rc::Rc;
+use wasm_bindgen::JsCast;
+use web_sys::{Element, Event, KeyboardEvent, MouseEvent};
+
+use std::fmt::Display;
+
 
 ////////////////////////////////////////////////////////////////////////////////////
 // --- functions ---
@@ -29,14 +38,95 @@ pub fn RateCurveComponent(
 ) -> impl IntoView {
     // α <fn rate_curve_component>
 
-    view! {
-        cx,
-
-        <div>"Rate Curve TODO"</div>
+    enum RateCurveTableTools {
+        Edit,
+        Remove,
+        AddNewYearValue
     }
 
-    // ω <fn rate_curve_component>
+
+    let plus_button = create_node_ref::<Button>(cx);
+    let minus_button = create_node_ref::<Button>(cx);
+    let edit_button = create_node_ref::<Button>(cx);
+
+    let mut year_value_vec = Vec::new();
+    let mut year_value_vec = vec![YearValue];
+    
+
+
+
+
+
+
+
+    let year_value_row = YearValue::new;//I don't think this actually works 
+
+    //fn add (onclickofplussign)
+   // fn remove(onclickofminussign) {year_value_vec.remove(year_value_row)}
+    //use year_value_vec.push(next_year_value)
+    //year_value_vec.remove(clicked minus sign)
+//create a variable that is like year_value_row, so you can add them, or subtract them
+//how to make year_value_row an element that includes YearValues
+<Button::plus_button>
+on::click
+    />
+
 }
+
+
+
+pub fn is_empty(&self) -> bool {
+    self.0.is_empty()
+}
+
+pub fn add(&mut self, AddNewYearValue: RateCurveTableTools) {
+    year_value_vec.0.push(year_value_row);
+}
+
+ pub fn remove(&mut self, id: Uuid) {
+     year_value_vec.retain(|year_value_row| year_value_row.id != id);}
+     
+
+
+ let mut year_value_vec = YearValue{year: 1, value: 42.0};
+      view! {
+        cx,
+         <div>"Rate Curve TODO"</div>
+        <main>
+        
+         </main>
+
+                
+
+        
+
+
+
+
+
+
+
+    
+    }
+
+
+
+
+
+    
+
+     // ω <fn rate_curve_component>
+
+
+
+
+
+
+
 
 // α <mod-def rate_curve_component>
 // ω <mod-def rate_curve_component>
+
+
+
+
