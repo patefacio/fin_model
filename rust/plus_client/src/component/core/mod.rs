d24632760e584738e50d5b8cee8805be107064da
Daniel Davidson <1096508+patefacio@users.noreply.github.com>
2023-09-20 10:52:32 -0500

    Move general components into core

---
+++ rust/plus_client/src/component/core/mod.rs
@@ -0,0 +1,27 @@
+//! Core components that are generally applicable in financial apps
+
+////////////////////////////////////////////////////////////////////////////////////
+// --- mod decls ---
+////////////////////////////////////////////////////////////////////////////////////
+pub mod collapsible_component;
+pub mod collection_grid_component;
+pub mod currency_select;
+pub mod currency_value_input;
+pub mod date_input;
+pub mod enum_select;
+pub mod integer_input;
+pub mod multi_column_select;
+pub mod normal_loss_component;
+pub mod normal_spec_component;
+pub mod numeric_input;
+pub mod ok_cancel_component;
+pub mod one_of_component;
+pub mod percent_input;
+pub mod year_currency_value_input;
+pub mod year_input;
+pub mod year_range_input;
+pub mod year_value_input;
+pub mod year_value_series_component;
+
+// α <mod-def core>
+// ω <mod-def core>
